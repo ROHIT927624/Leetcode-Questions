e6b6375832884869e073b9b4b7faa375ea8f52db
ROHIT927624 <154682791+ROHIT927624@users.noreply.github.com>
2026-07-28 12:37:30 +0530

    Time: 4 ms (91.01%) | Memory: 83 MB (36.65%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,13 +1,39 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        int ans=0;
+//        for(int i=0;i<nums.length-1;i++){
+//         if(nums[i]==nums[i+1]){
+//             ans= nums[i];
+//         }
+//        } 
+//        return ans;
+//     }
+// }
+
+
+// fast and slow pointer
 class Solution {
     public int findDuplicate(int[] nums) {
-       Arrays.sort(nums);
-       int ans=0;
-       for(int i=0;i<nums.length-1;i++){
-        if(nums[i]==nums[i+1]){
-            ans= nums[i];
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Find the meeting point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance of the cycle
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-       } 
-       return ans;
+
+        return slow;
     }
 }
